196ad9620538aaa372bba4e437cec49e3e33749f
wierzbickitomasz <72969906+wierzbickitomasz@users.noreply.github.com>
2021-03-16 11:28:10 +0100

    16_03_21
    16_03_21

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,10 @@
 
+const result = "a" + "\n" + "b";
 const techUsedDescriptions =
  [
-    ["b0e1","b0e1", "b0e2", "b0e3", , , ],
-    ["b1e0","b1e1", "b1e2", "b1e3", , , ],
-    ["b2e0","b2e1", "b2e2", "b2e3", , , ]
+    ["phaser 3 library (gamedev), classes, es6","index.html", "style", "b0e3", , , ],
+    ["Simple Java GUI","Java interface to OpenCV library (image processing)", "b1e2", "b1e3", , , ],
+    ["Primary Components: BrowserRouter, Route, Switch, Context","style", "b2e2", "b2e3", , , ]
   ];
 
 const techUsedContainer = document.querySelectorAll(".tech_used_container"); //belka, jest ich 3
@@ -14,12 +15,14 @@ for(let i = 0; i < techUsedContainer.length; i++) {   //iterujemy po belce
     for(let j = 0; j < ((techUsedContainer[i].querySelectorAll(".tech_used")).length); j++) { //iterujemy po poszczegolnych elementach belki
         techUsedContainer[i].querySelectorAll(".tech_used")[j].addEventListener("mouseover", function(event) {
             console.log("belka:"+ i + "element w belce:" + j);
-            techUsedDescriptionText[i].style.visibility = "visible";
+            //techUsedDescriptionText[i].style.visibility = "visible";
+            techUsedDescriptionText[i].style.opacity="1"
+            
             techUsedDescriptionText[i].textContent = techUsedDescriptions[i][j];
 
         })
         techUsedContainer[i].querySelectorAll(".tech_used")[j].addEventListener("mouseout", function(event) {
-            techUsedDescriptionText[i].style.visibility = "hidden";
+            techUsedDescriptionText[i].style.opacity="0"
 
         })
   
--- js/technologies.js
+++ js/technologies.js
@@ -1,25 +1,23 @@
 const technologies = document.querySelectorAll(".technology");
 const technologiesDescriptionText = document.querySelector(".technologies_description_text");
 const technologiesDescription = document.querySelector(".technologies_description");
-const descriptions = ["Tu jest opis moich umiejętności w wybranej technologii 1", "Tu jest opis moich umiejętności w wybranej technologii 2", "Tu jest opis moich umiejętności w wybranej technologii 3", "Tu jest opis moich umiejętności w wybranej technologii 4"];
+const descriptions = ["I know how to create responsive website using Flexbox and Media queries", "I create my apps in es6, and i use OOP in javascript", "I use  html 5 capabilities", "Java is the object-oriented language that I used the most in my studies", "I create apps in React, but im still learning. I'm doing next, more advanced course"];
 for(let i = 0; i < technologies.length; i++) {
-   //h1Elements[i].style.color = "#fff";
    technologies[i].addEventListener("mouseover", function(event) {
-    setVisibleTechDescription();
-    //event.target.textContent = "aaa";
     technologiesDescriptionText.textContent = descriptions[i];
-    //console.log(this.id);
+    technologiesDescription.classList.add("visible");
+    //setVisibleTechDescription();
 });
-
     technologies[i].addEventListener("mouseout", function(event) {
-        technologiesDescriptionText.textContent = "...";
-    setInvisibleTechDescription();
+    technologiesDescription.classList.remove("visible");
+
 });
 
 }
 function setVisibleTechDescription() {
-    technologiesDescription.style.display = "flex";
+
+    
 }
 function setInvisibleTechDescription() {
-    technologiesDescription.style.display = "none";
+   
 }
